f1f04b544126f71cafc8c6151fc7444a7e2b02bb
Kim Joongil <kji7673@gmail.com>
2026-08-13 19:16:10 +0900

    fix: pass serverFunction to RootLayout to resolve ServerFunctionsProvider error

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -1,13 +1,31 @@
 import config from '@payload-config'
-import { RootLayout } from '@payloadcms/next/layouts'
-import React from 'react'
 import '@payloadcms/next/css'
+import type { ServerFunctionClient } from 'payload'
+import {
+  handleServerFunctions,
+  RootLayout,
+} from '@payloadcms/next/layouts'
+import React from 'react'
+
 import { importMap } from './admin/importMap.js'
 
 type Args = {
   children: React.ReactNode
 }
 
-export default function Layout({ children }: Args) {
-  return <RootLayout config={config} importMap={importMap}>{children}</RootLayout>
+const serverFunction: ServerFunctionClient = async function (args) {
+  'use server'
+  return handleServerFunctions({
+    ...args,
+    config,
+    importMap,
+  })
 }
+
+const Layout = ({ children }: Args) => (
+  <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
+    {children}
+  </RootLayout>
+)
+
+export default Layout
