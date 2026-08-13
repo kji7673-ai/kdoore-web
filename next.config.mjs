8a31bf4ce012a31e61cc8fad1fb04ff7818fb144
Kim Joongil <kji7673@gmail.com>
2026-08-13 17:25:36 +0900

    fix: Remove manual importMap.js to allow Payload to auto-generate it during build

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@ import { withPayload } from '@payloadcms/next/withPayload';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    PGSSLMODE: 'no-verify',
+  },
   images: {
     remotePatterns: [
       {
