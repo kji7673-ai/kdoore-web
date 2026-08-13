4de12b133bc091e29752a2ae9e855a8c67608dc2
Kim Joongil <kji7673@gmail.com>
2026-08-13 13:21:50 +0900

    fix: Payload CMS importMap missing error causing Hydration Mismatch

--- src/app/(payload)/admin/[[...segments]]/page.tsx
+++ src/app/(payload)/admin/[[...segments]]/page.tsx
@@ -1,5 +1,6 @@
 import config from '@payload-config'
 import { RootPage, generatePageMetadata } from '@payloadcms/next/views'
+import { importMap } from '../importMap.js'
 
 type Args = {
   params: Promise<{
@@ -14,6 +15,6 @@ export const generateMetadata = ({ params, searchParams }: Args) =>
   generatePageMetadata({ config, params, searchParams })
 
 const Page = ({ params, searchParams }: Args) =>
-  RootPage({ config, params, searchParams })
+  RootPage({ config, params, searchParams, importMap })
 
 export default Page
--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -2,11 +2,12 @@ import config from '@payload-config'
 import { RootLayout } from '@payloadcms/next/layouts'
 import React from 'react'
 import '@payloadcms/next/css'
+import { importMap } from './admin/importMap.js'
 
 type Args = {
   children: React.ReactNode
 }
 
 export default function Layout({ children }: Args) {
-  return <RootLayout config={config}>{children}</RootLayout>
+  return <RootLayout config={config} importMap={importMap}>{children}</RootLayout>
 }
