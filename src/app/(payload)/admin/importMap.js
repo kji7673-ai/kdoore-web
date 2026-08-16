06e08ca809fc83bdb289264715246d64e6bf6836
Kim Joongil <kji7673@gmail.com>
2026-08-16 09:06:07 +0900

    fix: update importmap with VercelBlobClientUploadHandler

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -1,7 +1,9 @@
 import { RscEntryLexicalCell, RscEntryLexicalField, LexicalDiffComponent } from '@payloadcms/richtext-lexical/rsc'
+import { VercelBlobClientUploadHandler } from '@payloadcms/storage-vercel-blob/client'
 
 export const importMap = {
   '@payloadcms/richtext-lexical/rsc#RscEntryLexicalCell': RscEntryLexicalCell,
   '@payloadcms/richtext-lexical/rsc#RscEntryLexicalField': RscEntryLexicalField,
   '@payloadcms/richtext-lexical/rsc#LexicalDiffComponent': LexicalDiffComponent,
+  '@payloadcms/storage-vercel-blob/client#VercelBlobClientUploadHandler': VercelBlobClientUploadHandler,
 }
